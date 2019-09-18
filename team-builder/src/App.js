512eddc2cd1f62d87b83834b547e974ad4895e11
chudnofskyATD <mpereannor@gmail.com>
2019-09-18 12:32:44 +0000

    setting up App structure v1

---
+++ team-builder/src/App.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react';
+import './App.css';
+
+// ReactDOM.render(
+//   <Container />,
+//   document.querySelector('#target'),
+// );
+
+function App() {
+  return (
+
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
