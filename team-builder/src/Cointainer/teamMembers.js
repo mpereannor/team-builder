9694286218698ef9ae8bf378bd89ca821cfa8a57
chudnofskyATD <mpereannor@gmail.com>
2019-09-18 13:43:22 +0000

    declaration of default values

--- team-builder/src/Cointainer/teamMembers.js
+++ team-builder/src/Cointainer/teamMembers.js
@@ -1,10 +1,24 @@
+import React, { useState } from 'react';
+import uuid from 'uuid';
 
+//declaration of initial values 
+const initTeamList = 
+[
+{id: uuid(), name: 'Leo Messi', age:'32', nationality: 'Argentina', marketValue: '150 million'}
+]
+
+const initFormValue = {
+    name: '',
+    age: '',
+    nationality: '',
+    marketValue: ''
+}
 
 export default function  EntireTeam () {
 
 //declaration of slices of state
-const [teamList, setTeamList] = useState({});
-const [formState, setFormState] = useState([]);
+const [teamList, setTeamList] = useState(initTeamList);
+const [formState, setFormState] = useState(initFormValue);
 
 
 }
